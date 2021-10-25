3034f7771132be1382f5c6b02e3c0c9676c94f38
roqytis <91776641+roqytis@users.noreply.github.com>
2021-10-25 23:41:48 +0900

    JDBC_MyBatis_Ver_1

---
+++ JDBC_MyBatis_Ver_1/src/OralceMyBatisMain.java
@@ -0,0 +1,21 @@
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.dto.Dept;
+import com.exception.RecordNotFoundException;
+import com.service.OracleMyBatisService;
+
+public class OralceMyBatisMain {
+
+	public static void main(String[] args) {
+		OracleMyBatisService service= new OracleMyBatisService();
+		
+		List<Dept> list= service.selectAll();
+		for (Dept dept : list) {
+			System.out.println(dept);
+		}
+
+	}
+
+}
